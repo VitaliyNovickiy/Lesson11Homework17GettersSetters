31654afeb4bcb6c28836bf7940ee69772ad340f8
VitaliyNovickiy <vidmarev@mail.ru>
2023-06-30 21:05:54 +0300

    FirstCommit

--- Src/FitnessTracker.java
+++ Src/FitnessTracker.java
@@ -1,29 +1,33 @@
 
 public class  FitnessTracker {
-    private final String name;
-    private final int birthDay;
-    private final int birthMonth;
-    private final int birthYear;
-    private final String email;
-    private final String phone;
+    private  String name;
     public String surname;
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+    private  String email;
+    private  String phone;
     public double weight;
     public String pressure;
     public int steps;
-    private final int age;
-    public FitnessTracker(String name, int birthDay, int birthMonth, int birthYear, String email, String phone) {
+    private  int age;
+
+    public FitnessTracker(String name,String surname, int birthDay, int birthMonth, int birthYear,
+                          String email, String phone,double weight,String pressure, int steps) {
         this.name = name;
+        this.surname = surname;
         this.birthDay = birthDay;
         this.birthMonth = birthMonth;
         this.birthYear = birthYear;
         this.email = email;
         this.phone = phone;
-        this.surname = "";
-        this.weight = 0;
-        this.pressure = "";
-        this.steps = 0;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.steps = steps;
         this.age = calculateAge();
     }
+
+
 
     private int calculateAge() {
         int currentYear = 2020;
@@ -31,20 +35,39 @@ public class  FitnessTracker {
     }
 
 
+    @Override
+    public String toString() {
+        return "FitnessTracker " +
+                "name: " + name + '\'' +
+                ", surname: " + surname + '\'' +
+                ", birthDay: " + birthDay +
+                ", birthMonth: " + birthMonth +
+                ", birthYear: " + birthYear + '\n' +
+                " email: " + email + '\'' +
+                ", phone: " + phone + '\'' +
+                ", weight: " + weight +
+                ", pressure: " + pressure + '\'' +
+                ", steps: " + steps +
+                ", age: " + age +'\n' + '\n';
+    }
 
     public void printAccountInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Surname: " + surname);
-        System.out.println("Date of Birth: " + birthDay + "/" + birthMonth + "/" + birthYear);
-        System.out.println("Age: " + age);
-        System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
-        System.out.println("Weight: " + weight);
-        System.out.println("Blood Pressure: " + pressure);
-        System.out.println("Steps: " + steps);
-        System.out.println();
 
+        System.out.printf(toString());
 
     }
+
+    public String getName() {return name;}
+    public int getBirthDay() {return birthDay;}
+    public int getBirthMonth() {return birthMonth;}
+    public int getBirthYear() {return birthYear;}
+    public String getEmail() {return email;}
+    public String getPhone() {return phone;}
+    public int getAge() {return age;}
+
+    public void setSurname(String surname) {this.surname = surname;}
+    public void setWeight(double weight) {this.weight = weight;}
+    public void setPressure(String pressure) {this.pressure = pressure;}
+    public void setSteps(int steps) {this.steps = steps;}
 }
 
--- Src/Program.java
+++ Src/Program.java
@@ -1,28 +1,28 @@
 public class Program {
     public static void main(String[] args) {
 
-                FitnessTracker user1 = new FitnessTracker("John", 15, 6, 1990,
-                        "john@mail.com", "1234567890");
-                user1.surname = "Smith";
-                user1.weight = 65;
-                user1.pressure = "110/80";
-                user1.steps = 5625;
-                user1.printAccountInfo();
+                FitnessTracker user1 = new FitnessTracker("Alice", "Simson",1,12,
+                        1985, "alice.gmail.com","5252514",58,120+"/"+80,12000);
+               user1.printAccountInfo();
 
-                FitnessTracker user2 = new FitnessTracker("Alice", 10, 9, 1985,
-                        "alice@mail.com", "9876543210");
-                user2.surname = "Smith";
-                user2.weight = 50;
-                user2.pressure = "110/70";
-                user2.steps = 10000;
+               user1.setSteps(10500);
+               user1.setWeight(68);
+               user1.printAccountInfo();
+
+
+                FitnessTracker user2 = new FitnessTracker("Bob", "Peterson",2,10,
+                        1988, "bob.gmail.com","53232514",78,120+"/"+70,10000);
                 user2.printAccountInfo();
 
-                FitnessTracker user3 = new FitnessTracker("Bob", 20, 3, 2000,
-                        "bob@mail.com", "5555555555");
-                user3.surname = "James";
-                user3.weight = 99;
-                user3.pressure = "120/70";
-                user3.steps = 8852;
+                user2.setSurname("Bronson");
+                user2.setWeight(63);
+                user2.setPressure("120/100");
+                user2.printAccountInfo();
+
+
+                FitnessTracker user3 = new FitnessTracker("Andriy", "Jameson",10,9,
+                        1977, "andriy.gmail.com","53356514",85,110+"/"+70,1000);
                 user3.printAccountInfo();
+
     }
 }
